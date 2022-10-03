91147c20c2da2f2b5ca71a8d4f237b9379fc18ff
ChronicleCake <5543@holbertonstudents.com>
2022-10-02 19:10:51 -0700

    c is 1

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _islower - Entry point
+ * Return: 0 (Success)
+ *
+ */
+
+int _islower(int c)
+{
+	int ch = c;
+
+	if (ch == c)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
