45e311fa4848bfe9769b9c4ff34320865bdfe854
ChronicleCake <5543@holbertonstudents.com>
2022-10-03 16:03:54 -0700

    adding to get 98

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - increases by a number
+ * @n: variables
+ * @c: variables
+ * Return: n and c
+ */
+
+int add(int c, int n)
+{
+	int add;
+
+	add = (c + n);
+		return (add);
+}
