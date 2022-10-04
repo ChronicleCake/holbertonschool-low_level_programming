5d9fc453a196924ad5f5861cc434bd3f1e3383cd
ChronicleCake <5543@holbertonstudents.com>
2022-10-03 18:40:39 -0700

    is c a digit?

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isdigit - prints a digit
+ * @c: variable
+ * Return: 1 if c, otherwise 0
+ */
+
+int _isdigit(int c)
+{
+	if ((c >= 0) || (c <= 0))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
