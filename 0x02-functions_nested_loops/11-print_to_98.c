f28ec2e067b608428fb293223e6fc8940aa82d20
ChronicleCake <5543@holbertonstudents.com>
2022-10-03 17:56:17 -0700

    to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - print all from n to 98
+ * @n: variables
+ * Return: void
+ */
+
+void print_to_98(int n)
+{
+	for (n = n; n <= 98; n++)
+		printf("%d, ", n);
+	for (n = n; n >= 98; n--)
+		printf("%d, ", n);
+	printf("98\n");
+}
