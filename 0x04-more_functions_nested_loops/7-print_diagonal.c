c52b6e6086c10d493488bd902a89d143f2206cf2
ChronicleCake <5543@holbertonstudents.com>
2022-10-04 12:07:23 -0700

    white space

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_diagonal - entry point
+ * @n: variable
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	int ch;
+	int l;
+
+	for (ch = 1; ch <= n; ch++)
+	{
+		for (l = 0; l <= ch; l++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
