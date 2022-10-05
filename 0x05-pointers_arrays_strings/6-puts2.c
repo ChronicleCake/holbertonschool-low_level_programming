1e8599b2b15f7ce9f363c87df44fe3f12133e5bd
ChronicleCake <5543@holbertonstudents.com>
2022-10-05 10:41:31 -0700

    just add a null

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -3,4 +3,20 @@
 /**
  * puts2 - will print both
  * @str: will refrence a string from another file
- *
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int i = 0, ch;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	for (ch = 0; ch < i; ch += 2)
+	{
+		_putchar(str[ch]);
+	}
+	_putchar('\n');
+}
